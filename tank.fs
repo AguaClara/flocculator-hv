d1a5484442bf73f0fac2019d17e2b7ad6c6a3009
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-06-06 20:11:06 +0000

    tank.fs :: Edit FeatureScript

--- tank.fs
+++ tank.fs
@@ -54,7 +54,7 @@ export const tankDesigner = function(design) returns map
             {
                 direction = 0;
             }
-            rotate = append(rotate, direction * 180*degree);
+            design.rotate = append(design.rotate, direction * 180*degree);
         }
         return design;
     };
