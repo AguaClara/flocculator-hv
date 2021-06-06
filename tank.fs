7156fb8ff89b37c6aa82a552e9b7d95abd86f74b
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-06-06 20:54:55 +0000

    tank.fs :: Edit FeatureScript

--- tank.fs
+++ tank.fs
@@ -43,8 +43,8 @@ export const tankInstantiator = function(definition is map) returns map
 export const tankDesigner = function(design) returns map
     {
         design.channelB = design.channelW + design.channelWallT;
-        design.odd = max(floor((design.channelN+1)/2),0);
-        design.even = max(floor((design.channelN-2) /2),0);
+        design.odd = max(floor((design.channelN)/2),0);
+        design.even = max(floor((design.channelN)/2),0);
         return design;
     };
 
