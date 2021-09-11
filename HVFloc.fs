bb65acf1f87c698cbcb7b0320120e97faa024f56
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-09-11 21:02:01 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -137,6 +137,7 @@ export const hvFlocPreDesigner = function(design) returns map
         // find the maximum number of baffles spaces in each channel assuming that we need an even number in each channell
         // except the last channel where the inlet is low and the outlet is high and thus we need an odd number\
         // we will figure out the last channel by simply deleting the last baffle
+        design.baffle.T = 0.003 * meter;
         design.baffle.spacesN = floor(design.L / (design.baffle.S + design.baffle.T) / 2) * 2;
 
 
