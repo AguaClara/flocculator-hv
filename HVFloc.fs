6f7d16d6664328088334e922fffb8cc48e901449
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-09-11 21:58:31 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -162,20 +162,20 @@ export const hvFlocFeature = defineFeature(function(context is Context, id is Id
 function baffleS(design)
 {
     design.baffle = {};
-    design.KE = baffleKE(design.maxHS_pi);
+    //design.KE = baffleKE(design.maxHS_pi);
     var err = 1.0;
-    design.baffle.expH_max = OptimalHE(design);
-    design.S = (design.KE / (2 * design.baffle.expH * design.G ^ 2 * design.NU)) ^ (1 / 3) * design.Qm_max / design.channelW; //first guess
-    println("S is " ~ design.S);
-    println("H/S is" ~ design.baffle.expH / design.S);
-    var prevS = S;
+    //design.baffle.expH_max = OptimalHE(design);
+    //design.S = (design.KE / (2 * design.baffle.expH * design.G ^ 2 * design.NU)) ^ (1 / 3) * design.Qm_max / design.channelW; //first guess
+    //println("S is " ~ design.S);
+    //println("H/S is" ~ design.baffle.expH / design.S);
+    var prevS = 0 * meter;
     var count = 0;
     while ((err > 0.0001) && (count < 200))
     {
         count += 1;
         prevS = design.S;
 
-        design.baffle.expH_max = OptimalHE(design);
+        
         design.expN = ceil(design.outletHW / design.baffle.expH_max); // expansions per baffle
         design.baffle.expH = design.outletHW / design.expN; //distance between expansions
         design.KE = baffleKE(design.baffle.expH / prevS);
