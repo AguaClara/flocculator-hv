36a43c34f9c147a532c3d9ee679b3220acd084a5
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-09-11 22:01:51 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -165,10 +165,10 @@ function baffleS(design)
     //design.KE = baffleKE(design.maxHS_pi);
     var err = 1.0;
     design.baffle.expH_max = OptimalHE(design);
-    //design.S = (design.KE / (2 * design.baffle.expH * design.G ^ 2 * design.NU)) ^ (1 / 3) * design.Qm_max / design.channelW; //first guess
+    design.S = design.baffle.expH_max/design.maxHS_pi ; //first guess
     //println("S is " ~ design.S);
     //println("H/S is" ~ design.baffle.expH / design.S);
-    var prevS = 0 * meter;
+    var prevS = design.S;
     var count = 0;
     while ((err > 0.0001) && (count < 200))
     {
