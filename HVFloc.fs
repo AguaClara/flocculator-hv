5ab4ed7d1a4bdae8d6f0448db87620c661291b54
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-09-11 21:43:13 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -176,6 +176,7 @@ function baffleS(design)
         KE = baffleKE(design.baffle.expH / prevS);
         S = (KE / (2 * design.baffle.expH * design.G ^ 2 * design.NU)) ^ (1 / 3) * design.Qm_max / design.channelW;
         println("S is " ~ S);
+        println("H/S is" ~ design.baffle.expH/S);
         err = abs((S - prevS) / (S + prevS));
     }
     return S;
