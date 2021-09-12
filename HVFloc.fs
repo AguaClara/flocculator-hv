85aba22ea380fd2025c4b1d722adde645ef7c6d6
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-09-12 10:26:47 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -103,7 +103,7 @@ export const hvFlocPreDesigner = function(design) returns map
         design.KE = baffleKE(design.maxHS_pi);
 
         design = baffleS(design);
-        design.HS_pi = design.baffle.expH / design.baffle.S;
+        
         //design.tankW = (design.channelW + design.channelWallT) * design.channelN - design.channelWallT;
         //rework everything below
 
@@ -165,26 +165,27 @@ function baffleS(design)
     //design.KE = baffleKE(design.maxHS_pi);
     var err = 1.0;
     design.baffle.expH_max = OptimalHE(design);
-    design.S = design.baffle.expH_max/design.maxHS_pi ; //first guess
+    design.baffle.S = design.baffle.expH_max/design.maxHS_pi ; //first guess
     design.expN = ceil(design.outletHW / design.baffle.expH_max); // expansions per baffle
     //println("S is " ~ design.S);
     //println("H/S is" ~ design.baffle.expH / design.S);
-    var prevS = design.S;
+    var prevS = design.baffle.S;
     var count = 0;
     while ((err > 0.0001) && (count < 200))
     {
         count += 1;
-        prevS = design.S;
+        prevS = design.baffle.S;
 
         
         
         design.baffle.expH = design.outletHW / design.expN; //distance between expansions
         design.KE = baffleKE(design.baffle.expH / prevS);
-        design.S = (design.KE / (2 * design.baffle.expH * design.G ^ 2 * design.NU)) ^ (1 / 3) * design.Qm_max / design.channelW;
-        println("S is " ~ design.S);
-        println("H/S is" ~ design.baffle.expH / design.S);
-        err = abs((design.S - prevS) / (design.S + prevS));
+        design.baffle.S = (design.KE / (2 * design.baffle.expH * design.G ^ 2 * design.NU)) ^ (1 / 3) * design.Qm_max / design.channelW;
+        println("S is " ~ design.baffle.S);
+        println("H/S is" ~ design.baffle.expH / design.baffle.S);
+        err = abs((design.baffle.S - prevS) / (design.baffle.S + prevS));
     }
+    design.HS_pi = design.baffle.expH / design.baffle.S;
     return design;
 }
 
