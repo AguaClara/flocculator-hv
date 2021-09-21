f9e090ee5416986a482f134bbff29d1e445264a6
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-21 18:43:36 +0000

    Baffle.fs :: Edit FeatureScript

--- Baffle.fs
+++ Baffle.fs
@@ -90,8 +90,8 @@ export const bafflePreDesigner = function(design) returns map
 
         design.bafflebottomN = ceil(design.baffleN / 2); //number of bottom baffles
         design.baffletopN = floor(design.baffleN / 2); //number of top baffles
-        design.bafflefloorbottomS = 0 * meter; //distance between bottom of baffle and tank bottom (bottom baffle)
-        design.bafflefloortopS = design.baffleS; //distance between bottom of baffle and tank bottom (top baffle)
+        design.floorbottomS = 0 * meter; //distance between bottom of baffle and tank bottom (bottom baffle)
+        design.floortopS = design.baffleS; //distance between bottom of baffle and tank bottom (top baffle)
 
 
         //holes - top & bottom
