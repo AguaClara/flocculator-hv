e02fc5f45c4221e64ba148cf0597a1fef7bd49ae
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-23 18:32:34 +0000

    Baffle.fs :: Edit FeatureScript

--- Baffle.fs
+++ Baffle.fs
@@ -75,7 +75,7 @@ export const bafflePreDesigner = function(design) returns map
         //sheet
         design.bafflebottomL = design.tankH - design.FB - design.HL_bod - design.baffleS; //length of bottom baffle
         design.baffletopL = design.tankH - design.FB / 2; //length of top baffle
-        design.baffleN = floor(design.channelL / design.baffleS); //total number of baffles
+        design.baffleN = floor(design.channelL / design.baffleS) - 1; //total number of baffles
 
         if (design.lastchannel == true)
         {
