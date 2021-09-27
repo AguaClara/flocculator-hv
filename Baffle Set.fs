7d552b2d365cd6cd9a7f755ec20f7fd80e4ac790
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-27 00:34:13 +0000

    Baffle Set.fs :: Edit FeatureScript

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -13,8 +13,8 @@ export const bafflesetTree =
             textbooklink : "",
         },
         designers : {
-            pre : bafflePreDesigner,
-            post : bafflePostDesigner,
+            pre : bafflesetPreDesigner,
+            post : bafflesetPostDesigner,
         },
         params : {
             rep : true,
@@ -30,7 +30,6 @@ export const bafflesetTree =
             HL_bod : [0, 0.4, 1], //head loss, defined in parent
             washerT : [0.001, 0.003175, 0.2], //washer thickness
             washerOD : [0.125, 1, 3], //washer outer diameter
-        //washer diameter
 
         },
         children : {
@@ -43,7 +42,46 @@ export const bafflesetTree =
                     t : "corrugated", //type
                     mat : "PC", //material
                     ip : "$.ip", //implementation partner
+                    
+                    
+            rep : true,
+            ip : "app",
+            flowfront : false, //if water comes in at the bottom and moves from to back, hitting the baffles at the front
+            lastchannel : false,
+            tankH : [1, 2, 200], //height of tank, will be defined in parent via tank info
+            channelW : [0.05, 0.5, 100], //width of channel, will be defined in parent via tank info
+            channelL : [0, 7, 200], //length of channel, will be defined in parent via tank info
+            FB : [0, 0.1, 1], //free board, will be defined in parent generally
+            baffleT : [0, 0.0008, 2], //baffle thickness, will be defined in parent generally
+            baffleS : [0.01, 0.1, 10], //baffle spacing, will be calculated in parent
+            HL_bod : [0, 0.4, 1], //head loss, defined in parent
+            washerT : [0.001, 0.003175, 0.2], //washer thickness
+            washerOD : [0.125, 1, 3], //washer outer diameter
                 },
             },
         },
     };
+    
+    export const bafflesetPreDesigner = function(design) returns map
+    {
+
+        return design;
+
+    };
+
+export const bafflesetPostDesigner = function(design) returns map
+    {
+        
+         return design;
+
+    };
+
+
+annotation { "Feature Type Name" : "Baffle Set" }
+export const bafflesetFeature = defineFeature(function(context is Context, id is Id, definition is map)
+    precondition
+    {
+    }
+    {
+        treeInstantiatorFeature(context, id, bafflesetTree as InputTree);
+    });
