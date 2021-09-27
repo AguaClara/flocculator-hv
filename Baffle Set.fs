4c86f48cab73f71d4040aede65d71b7ac130d249
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-27 15:26:11 +0000

    Baffle Set.fs :: Edit FeatureScript

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -4,6 +4,8 @@ import(path : "onshape/std/geometry.fs", version : "1589.0");
 import(path : "2fa81f50be25609bc956cd5f/9315fcf8489f0c0cc1a06a01/40a6bde79e4081741060af59", version : "24d9ce4bf05b3add5d64a574");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "73ef3187c0c0c31743c8266c");
 baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "c3aac59959c3a7d35b257c07");
+import(path : "c81fce53dede81ef89860aa3/e327e5cbd266d1bdaa593857/b453944163f91ccf1477e3f0", version : "089950d7abdafd6c56066cc3");
+
 
 
 
