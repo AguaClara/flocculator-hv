2eab4ceea6292847e3a8a4aa5d65b370f6453ac2
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-27 00:44:03 +0000

    Baffle PS :: Hide Front

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -1,7 +1,7 @@
 FeatureScript 1589;
 import(path : "onshape/std/geometry.fs", version : "1589.0");
 
-import(path : "c2f4584cf9d8b1114f7ff5b4", version : "941185318b43d11475257525");
+import(path : "c2f4584cf9d8b1114f7ff5b4", version : "63c1ed1132c9da74853ebd12");
 
 
 export const bafflesetTree =
