7b524ff827fff9c37c3fa02df2abc60be1918e94
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-28 21:26:35 +0000

    Baffle PS :: Edit : End Top Spacers

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -3,7 +3,7 @@ import(path : "onshape/std/geometry.fs", version : "1589.0");
 
 import(path : "2fa81f50be25609bc956cd5f/9315fcf8489f0c0cc1a06a01/40a6bde79e4081741060af59", version : "24d9ce4bf05b3add5d64a574");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "85850ebabbc641188e9efe11");
-baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "6063446277260638dfb1e11c");
+baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "de6d25cf7c9cff9813fb1cd4");
 import(path : "c81fce53dede81ef89860aa3/143d08de7750ac406af6ad04/b453944163f91ccf1477e3f0", version : "d3e1c56c5ddb796b94cd62ce");
 
 
