3bd385e69b8e9b4ae90d9f978aa627dce67043bf
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-09-28 16:18:10 +0000

    Baffle PS :: Edit : High Washers @ Top Baffle

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -3,7 +3,7 @@ import(path : "onshape/std/geometry.fs", version : "1589.0");
 
 import(path : "2fa81f50be25609bc956cd5f/9315fcf8489f0c0cc1a06a01/40a6bde79e4081741060af59", version : "24d9ce4bf05b3add5d64a574");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "73ef3187c0c0c31743c8266c");
-baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "5d37ed71f0a08a966b58bcc9");
+baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "a233b47a20c43e78d15e79f1");
 import(path : "c81fce53dede81ef89860aa3/143d08de7750ac406af6ad04/b453944163f91ccf1477e3f0", version : "d3e1c56c5ddb796b94cd62ce");
 
 
