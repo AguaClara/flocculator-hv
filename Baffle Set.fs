6da2ae7d130a97444448f3953626d78badb3a995
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-28 17:51:10 +0000

    Baffle PS :: Move : Rollback bar

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -3,7 +3,7 @@ import(path : "onshape/std/geometry.fs", version : "1589.0");
 
 import(path : "2fa81f50be25609bc956cd5f/9315fcf8489f0c0cc1a06a01/40a6bde79e4081741060af59", version : "24d9ce4bf05b3add5d64a574");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "2ef7b5fc83cd1ae3f0fa721f");
-baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "a3a85c1f289111551222fa1b");
+baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "9e63572ef7c35c6cc799376c");
 import(path : "c81fce53dede81ef89860aa3/143d08de7750ac406af6ad04/b453944163f91ccf1477e3f0", version : "d3e1c56c5ddb796b94cd62ce");
 
 
