e6afa6d24ee2abdf6fc19efc43be7e9a3cbecf8a
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-30 19:11:02 +0000

    Baffle PS :: Update external references

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -3,7 +3,7 @@ import(path : "onshape/std/geometry.fs", version : "1589.0");
 
 import(path : "2fa81f50be25609bc956cd5f/8447782876cbe95a017e2753/40a6bde79e4081741060af59", version : "8aa9c6c4240674e319355626");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "82525bb4acc3a74073781769");
-baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "2381ac770c829352ea4edd67");
+baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "b5c24c74f624d63c44c11b68");
 import(path : "c81fce53dede81ef89860aa3/bb45cd3912709c587e62e7fb/b453944163f91ccf1477e3f0", version : "d3e1c56c5ddb796b94cd62ce");
 
 
