7741c1e293fb3e79cf84421d294d160a7d9233db
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-30 18:31:51 +0000

    Baffle.fs :: Edit FeatureScript

--- Baffle.fs
+++ Baffle.fs
@@ -103,8 +103,8 @@ export const bafflePreDesigner = function(design) returns map
         //holes & pipe - top & bottom
         design.pipe = queryPipeDim(0.5, design.ip, true, 26, ["ND", "ID", "OD", "schedule"]);
     
-        design.pipe.fittingR = 0.02 * meter; //later delete    
-        //design.pipe.fittingR = (queryFittingDim(design.pipe.ND, design.ip, FittingType.CAP, FittingShape.CAP, ConnectionStyle.SOCKET_CONNECT, "OD")) / 2; //distance from node to outer cap, TBD
+        //design.pipe.fittingR = 0.02 * meter; //later delete    
+        design.pipe.fittingR = (queryFittingDim(design.pipe.ND, design.ip, "CAP", FittingShape.CAP, ConnectionStyle.SOCKET_CONNECT, "OD")) / 2; //distance from node to outer cap, TBD
         design.pipe.L = design.channelL - 2*design.pipe.fittingR; //length of pipe (node to node)
         design.pipe.colN = ceil(design.channelW / (0.25 * meter)); //random equation for number of pipe columns, TBD
         design.pipe.hedgeB = 0.1 * meter; //horizontal edge distance from middle of hole
