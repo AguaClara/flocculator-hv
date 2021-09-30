ab75e774d718e0e47da96a8be3569deef18692bd
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-30 20:06:20 +0000

    Baffle.fs :: Edit FeatureScript

--- Baffle.fs
+++ Baffle.fs
@@ -131,11 +131,10 @@ export const bafflePreDesigner = function(design) returns map
         design.spacer.lowerN = design.baffleN - 1; //number of lower spacers
         design.spacer.tobaffleS = design.washerT; //spacing between spacer & baffle
         
-        design.pipe.fittingL = queryFittingDim(design.pipe.ND, design.ip, FittingType.CAP, FittingShape.CAP, ConnectionStyle.SOCKET_CONNECT, ["socketL"]).socketL; //length of fitting, socketL, TBD
-        print(design.fittingL);
-        design.spacer.topbackL = design.baffleS*2 - design.fittingL; //length of top back spacers
-        design.spacer.botbackL = design.baffleS - design.fittingL; //length of bottom back spacers
-        design.spacer.front1D = design.channelL - 2*design.baffleB - design.fittingL;
+
+        design.spacer.topbackL = design.baffleS*2 - design.pipe.fitting.socketL; //length of top back spacers
+        design.spacer.botbackL = design.baffleS - design.pipe.fitting.socketL; //length of bottom back spacers
+        design.spacer.front1D = design.channelL - 2*design.baffleB - design.pipe.fitting.socketL;
         design.spacer.botfront2D = design.baffleB*(design.baffleN - 2) + design.washerT;
         
         if (design.lastchannel == true) //length of top front spacers depending on if last channel or not
