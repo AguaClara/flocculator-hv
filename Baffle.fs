1ad294f35bacb122e10d7195966ccfcdf940a1ad
61377102f5f1d7396ace3b3d <61377102f5f1d7396ace3b3d>
2021-09-30 20:15:56 +0000

    Baffle.fs :: Edit FeatureScript

--- Baffle.fs
+++ Baffle.fs
@@ -131,7 +131,7 @@ export const bafflePreDesigner = function(design) returns map
         design.spacer.tobaffleS = design.washerT; //spacing between spacer & baffle
         
 
-        design.spacer.topbackL = design.baffleS*2 - design.fitting.socketL - design.fitting.R; //length of top back spacers
+        design.spacer.topbackL = design.baffleS*2 - design.fitting.socketL - design.fitting.R + design.baffleT; //length of top back spacers
         design.spacer.botbackL = design.baffleS - design.fitting.socketL - design.fitting.R; //length of bottom back spacers
         design.spacer.front1D = design.channelL - 2*design.baffleB - design.fitting.socketL;
         design.spacer.botfront2D = design.baffleB*(design.baffleN - 2) + design.washerT;
