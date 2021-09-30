13b6d161d74d0920806efc7de19d85a5d0497434
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-09-30 23:29:43 +0000

    Baffle Set.fs :: Update 2 versions

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -1,11 +1,11 @@
 FeatureScript 1589;
 import(path : "onshape/std/geometry.fs", version : "1589.0");
 
-import(path : "2fa81f50be25609bc956cd5f/8447782876cbe95a017e2753/40a6bde79e4081741060af59", version : "8aa9c6c4240674e319355626");
+import(path : "2fa81f50be25609bc956cd5f/818b7dee375f1bc9bcc87be6/40a6bde79e4081741060af59", version : "f6009dd864bdb2390dd1205d");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "20ac542505d0a0c9c174c8d1");
 baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "7fe8b5f78230115c941ddc50");
 import(path : "c81fce53dede81ef89860aa3/bb45cd3912709c587e62e7fb/b453944163f91ccf1477e3f0", version : "d3e1c56c5ddb796b94cd62ce");
-import(path : "1802d3650943f2f88dc71465/30bd90c893b7bb6dce7af5ff/6bdb65f6d231bdb48c906ef5", version : "703e90a5cd55f506ff8b7c00");
+import(path : "1802d3650943f2f88dc71465/3a05b346bfa0063d72af5740/6bdb65f6d231bdb48c906ef5", version : "703e90a5cd55f506ff8b7c00");
 
 
 
