60b10468246236946b3e589b790aa0adc079a636
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-10-08 14:00:10 +0000

    Baffle.fs :: Update version : V10 std 1605

--- Baffle.fs
+++ Baffle.fs
@@ -1,6 +1,6 @@
 FeatureScript 1576;
 import(path : "onshape/std/geometry.fs", version : "1576.0");
-import(path : "2fa81f50be25609bc956cd5f/818b7dee375f1bc9bcc87be6/40a6bde79e4081741060af59", version : "f6009dd864bdb2390dd1205d");
+import(path : "2fa81f50be25609bc956cd5f/bf841534b537d44305a7974a/40a6bde79e4081741060af59", version : "d7913675a6d5c4015f6502be");
 
 
 
