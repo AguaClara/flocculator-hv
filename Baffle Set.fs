981d31b37f113ccdd4255bc250628e91eac7be93
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2021-10-09 20:11:51 +0000

    Baffle Set.fs :: Edit FeatureScript

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -1,13 +1,8 @@
-FeatureScript 1589;
-import(path : "onshape/std/geometry.fs", version : "1589.0");
+FeatureScript 1605;
+import(path : "2fa81f50be25609bc956cd5f/bbdfcd94cf0ba1f62aa60b22/40a6bde79e4081741060af59", version : "974cfa08700945e0e87b1bbb");
 
-import(path : "2fa81f50be25609bc956cd5f/818b7dee375f1bc9bcc87be6/40a6bde79e4081741060af59", version : "f6009dd864bdb2390dd1205d");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "acbbc8e9584790c38bc6b891");
 baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "975bcf6e0506c6b9560ae3eb");
-import(path : "c81fce53dede81ef89860aa3/bb45cd3912709c587e62e7fb/b453944163f91ccf1477e3f0", version : "d3e1c56c5ddb796b94cd62ce");
-import(path : "1802d3650943f2f88dc71465/3a05b346bfa0063d72af5740/6bdb65f6d231bdb48c906ef5", version : "703e90a5cd55f506ff8b7c00");
-
-
 
 
 
