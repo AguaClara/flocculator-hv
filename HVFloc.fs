3581b01323ae7b634cfa89db4e4591bc524fc94f
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-01-17 18:27:55 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -28,7 +28,7 @@ export const hvFlocTree = {
         params : {
             ip : "GENERIC",
             rep : false,
-            "Qm_max" : [1, 30, 200],
+            "Qm_max" : [1, 30, 500],
             "Q_pi" : [0, 1, 2],
             "L" : [1, 7, 20],
             "humanChannelW_min" : [0.3, 0.45, 1],
