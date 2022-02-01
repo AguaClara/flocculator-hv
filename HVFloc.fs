b65d41ab1f9f8781e07d89f699792bfb08fff3ce
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-02-01 15:23:06 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -167,11 +167,7 @@ export const hvFlocPreDesigner = function(design) returns map
 
 export const hvFlocPostDesigner = function(design) returns map
     {
-        design.OW = (design.channelW + design.tank.wall.T) * design.channelN;
-        if (design.etWall)
-        {
-            design.OW = design.OW + design.tank.wall.T;
-        }
+        design.OW = design.tank.OW;
         design.channelEven = floor(design.channelN / 2) == ceil(design.channelN / 2);
         return design;
     };
