01f6975bde625ca1a280d59d83faa654e1b3350f
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-04-02 17:13:49 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -38,7 +38,7 @@ export const hvFlocTree = {
             "HL_bod" : [0, 0.4, 1],
             "minHS_pi" : [3, 4, 5],
             "maxHS_pi" : [6, 8, 10],
-            "outletHW" : [0, 2, 5],
+            "outletHW" : [1, 2, 5],
             "GT_min" : [0, 35000, 100000],
             "FB" : [0.05, 0.1, 0.5],
             "G_bod" : [20, 50, 100],
