077bcb21a27489a7f69c162d171f763408406e67
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-04-08 19:32:20 +0000

    flocHV.fs :: Edit FeatureScript

--- flocHV.fs
+++ flocHV.fs
@@ -17,47 +17,47 @@ export type DemoParams typecheck canBeDemoParams;
 export predicate canBeDemoParams(definition)
 {
 
- annotation { "Name" : "Use configuration for booleans", "Default" : false }
-        definition.configBooleans is boolean;
+    annotation { "Name" : "Use configuration for booleans", "Default" : false }
+    definition.configBooleans is boolean;
 
-        if (definition.configBooleans)
-        {
-            annotation { "Name" : "Print map of inputs in FeatureScript notices", "Default" : "false" }
-            isAnything(definition.configprintParams);
+    if (definition.configBooleans)
+    {
+        annotation { "Name" : "Print map of inputs in FeatureScript notices", "Default" : "false" }
+        isAnything(definition.configprintParams);
 
-            annotation { "Name" : "Show internal components", "Default" : "true" }
-            isAnything(definition.configplastic);
+        annotation { "Name" : "Show internal components", "Default" : "true" }
+        isAnything(definition.configplastic);
 
-            annotation { "Name" : "Replicate all components", "Default" : "true" }
-            isAnything(definition.configrep);
-        }
-        else
-        {
-            annotation { "Name" : "Print map of inputs in FeatureScript notices", "Default" : false }
-            definition.printParams is boolean;
+        annotation { "Name" : "Replicate all components", "Default" : "true" }
+        isAnything(definition.configrep);
+    }
+    else
+    {
+        annotation { "Name" : "Print map of inputs in FeatureScript notices", "Default" : false }
+        definition.printParams is boolean;
 
-            annotation { "Name" : "Show internal components", "Default" : true }
-            definition.plastic is boolean;
+        annotation { "Name" : "Show internal components", "Default" : true }
+        definition.plastic is boolean;
 
-            annotation { "Name" : "Replicate all components", "Default" : true }
-            definition.rep is boolean;
-        }
+        annotation { "Name" : "Replicate all components", "Default" : true }
+        definition.rep is boolean;
+    }
 
 
-        annotation { "Name" : "Location(s)", "Filter" : BodyType.MATE_CONNECTOR, "MaxNumberOfPicks" : 1 }
-        definition.location is Query;
+    annotation { "Name" : "Location(s)", "Filter" : BodyType.MATE_CONNECTOR, "MaxNumberOfPicks" : 1 }
+    definition.location is Query;
 
-        annotation { "Name" : "From Mate Connector Name" }
-        definition.mcName is mateName;
+    annotation { "Name" : "From Mate Connector Name" }
+    definition.mcName is mateName;
 
-        annotation { "Name" : "Place design variables in context", "Default" : true }
-        definition.mapToContext is boolean;
+    annotation { "Name" : "Place design variables in context", "Default" : true }
+    definition.mapToContext is boolean;
 
-        if (definition.mapToContext)
-        {
-            annotation { "Name" : "sub map name", "Default" : "flocHV" }
-            definition.submap is string;
-        }
+    if (definition.mapToContext)
+    {
+        annotation { "Name" : "sub map name", "Default" : "flocHV" }
+        definition.submap is string;
+    }
 }
 
 export type FlocHVParams typecheck canBeFlocHVParams;
@@ -87,12 +87,8 @@ annotation { "Feature Type Name" : "Flocculator HV" }
 export const flocculatorHV = defineFeature(function(context is Context, id is Id, definition is map)
     precondition
     {
-
         canBeFlocHVParams(definition);
-
-canBeDemoParams(definition);
-       
-
+        canBeDemoParams(definition);
     }
     {
         if (definition.configBooleans)
