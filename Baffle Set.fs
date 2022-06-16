b442d469ccc04ec09e83401e3f25be795ae0db44
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-06-16 13:08:36 +0000

    Baffle Set.fs :: Edit FeatureScript

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -32,7 +32,7 @@ export const baffleSetTree =
             FB : [0, 0.1, 1],
             baffleT : [0, 0.0008, 2],
             baffleS : [0.01, 0.1, 10],
-            HL_bod : [0, 0.4, 1],
+            HL_max : [0, 0.4, 1],
 
         },
         children : {
@@ -49,7 +49,7 @@ export const baffleSetTree =
                     FB : "$.FB",
                     baffleT : "$.baffleT",
                     baffleS : "$.baffleS",
-                    HL_bod : "$.HL_bod",
+                    HL_max : "$.HL_max",
 
                 },
             },
