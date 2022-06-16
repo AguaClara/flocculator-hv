a2327c87f8e35f7e4488d513dd6c3340f10a0520
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-06-16 18:34:41 +0000

    HVFloc.fs :: Edit FeatureScript

--- HVFloc.fs
+++ HVFloc.fs
@@ -138,7 +138,7 @@ export const hvFlocPreDesigner = function(design) returns map
         if (design.W_total < design.channelW_min)
         {
             design.W_total = design.channelW_min;
-            design.L = design.VOL / (design.W_total * design.outletHW);
+            design.L = max(design.VOL / (design.W_total * design.outletHW),design.L_min);
         }
         // need to make sure we don't specify a channel that is wider than the polycarbonate sheets
         design.channelN = max([floor(design.W_total / design.channelW_min), ceil(design.W_total / design.baffleChannelW_max)]); //make sure we don't try zero channels
