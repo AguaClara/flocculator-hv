a3246d8c8ab2538e2c637022f95efbcf8ec6f903
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-07-06 20:28:03 +0000

    Baffle PS :: Move : Rollback bar

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -3,7 +3,7 @@ import(path : "5298e094b1acc40ce9092d44", version : "44b845c14febeeb507a06293");
 import(path : "c81fce53dede81ef89860aa3/a18f2bfe304c65a95a56e88a/b453944163f91ccf1477e3f0", version : "fba735a8b646b0a74fa9a5bc");
 
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "407b61f1266fced45802f548");
-baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "de6207766bf032dca5277501");
+baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "3eb28cbfc767992d94a15d83");
 
 
 
