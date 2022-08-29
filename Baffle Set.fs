92f0e9f71738845753eff490a85960c36708ddee
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-08-29 18:28:27 +0000

    Baffle Set.fs :: Update version : V73 add default for mcModule as ""

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -1,6 +1,6 @@
 FeatureScript 1793;
 import(path : "5298e094b1acc40ce9092d44", version : "c13693e8fa112f9c01658354");
-import(path : "c81fce53dede81ef89860aa3/65ee6f411389e78fc74f73eb/b453944163f91ccf1477e3f0", version : "1ffe2e7251733551c7ab652c");
+import(path : "c81fce53dede81ef89860aa3/d4326a5f87def727eee34c22/b453944163f91ccf1477e3f0", version : "f9e1e38f472c22cb921f7cfe");
 
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "4f09ccb80792178bdf31f1ad");
 baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "dafda0d6616245fafb223bfc");
