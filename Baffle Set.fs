6c1272a5f99b25659a86be07af68cdeac2c1bc8b
5b8801819912ea0fe9b4d8ac <5b8801819912ea0fe9b4d8ac>
2022-09-13 15:39:00 +0000

    Baffle Set.fs :: Update version : V75 fix serializer handling of arrays

--- Baffle Set.fs
+++ Baffle Set.fs
@@ -1,6 +1,6 @@
 FeatureScript 1793;
 import(path : "5298e094b1acc40ce9092d44", version : "6165ead19f74760e1d1c57ff");
-import(path : "c81fce53dede81ef89860aa3/d4326a5f87def727eee34c22/b453944163f91ccf1477e3f0", version : "f9e1e38f472c22cb921f7cfe");
+import(path : "c81fce53dede81ef89860aa3/56e66e39ea1298eacd268600/b453944163f91ccf1477e3f0", version : "b584ae1d047a3595e99a2ba7");
 import(path : "ff0f26334dc9ed5e1dbbc027", version : "67729e1bcbfbe33738205576");
 baffleModule::import(path : "c2f4584cf9d8b1114f7ff5b4", version : "0df0d24547a2eb0bb6c291f5");
 
